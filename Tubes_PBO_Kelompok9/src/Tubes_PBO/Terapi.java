ea624e44131eee19c4e9cc95dc1dec59e5565630
ariqohn <96010719+ariqohn@users.noreply.github.com>
2021-12-16 14:40:39 +0700

    Update Terapi.java

--- Tubes_PBO_Kelompok9/src/Tubes_PBO/Terapi.java
+++ Tubes_PBO_Kelompok9/src/Tubes_PBO/Terapi.java
@@ -10,5 +10,31 @@ package Tubes_PBO;
  * @author Naufal
  */
 public class Terapi {
+    private string jenis_terapi;
+    private string deskripsi_penyakit;
+    private string jenis_penyakit;
     
+    public void setJenis_terapi(String jenis_terapi){
+        this.jenis_terapi = jenis_terapi;
+    }
+    public void setDeskripsi_penyakit(String deskripsi_penyakit){
+        this.deskripsi_penyakit = deskripsi_penyakit;
+    }
+    public void setJenis_penyakit(String jenis_penyakit){
+        this.jenis_penyakit = jenis_penyakit;
+    }
+    
+    public String getJenis_terapi(){
+        return this.jenis_terapi;
+    }
+    public String getDeskripsi_penyakit(){
+        return this.deskripsi_penyakit;
+    }
+    public String getJenis_penyakit(){
+        return this.jenis_penyakit;
+    }
+    
+    public String informasi_terapi(){
+        return this.informasi_terapi;
+    }
 }
