e5cc346734036e3a61a59e06eb04bff3deda89a8
ariqohn <96010719+ariqohn@users.noreply.github.com>
2021-12-16 15:21:34 +0700

    Update Farmasi.java

--- Tubes_PBO_Kelompok9/src/Tubes_PBO/Farmasi.java
+++ Tubes_PBO_Kelompok9/src/Tubes_PBO/Farmasi.java
@@ -15,5 +15,36 @@ public class Farmasi {
     private string deskripsi_obat;
     private string kode_pasien;
     
+    public void setKode_obat(String kode_obat){
+        this.kode_obat = kode_obat;
+    }
+    public void setNama_obat(String nama_obat){
+        this.nama_obat = nama_obat;
+    }
+    public void setDeskripsi_obat(String deskripsi_obat){
+        this.deskripsi_obat = deskripsi_obat;
+    }
+    public void setKode_pasien(String kode_pasien){
+        this.kode_pasien = kode_pasien;
+    }
     
+    public String getKode_obat(){
+        return this.kode_obat;
+    }
+    public String getNama_obat(){
+        return this.nama_obat;
+    }
+    public String getDeskripsi_obat(){
+        return this.deskripsi_obat;
+    }
+    public String getKode_pasien(){
+        return this.kode_pasien;
+    }
+    
+    public void display(){
+        System.out.println("Kode Obat    :" + this.kode_obat);
+        System.out.println("Nama Obat :" + this.nama_obat);
+        System.out.println("Deskripsi Obat     :" + this.deskripsi_obat;
+        System.out.println("Kode Pasien         :" + this.kode_pasien);
+    }
 }
